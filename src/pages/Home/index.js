8e44d539dbd5ff576c110fcea28d9378cb56a907
Guilherme GDP (DevGDP) <33395796+devgdp@users.noreply.github.com>
2021-04-12 19:32:01 -0300

    Completo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   useEffect(()=>{
 
@@ -13,11 +14,20 @@ export default function Home() {
         const response = await api.get('r-api/?api=filmes')
         // console.log(response.data);
         setFilmes(response.data);
+        setLoading(false);
     }
 
     loadFilmes();
 
   }, []);
+
+  if(loading){
+    return(
+        <div className="filme-info">
+            <h1>Carregando Filmes...</h1>
+        </div>
+    );  
+}
 
   return (
     <div className="container">
